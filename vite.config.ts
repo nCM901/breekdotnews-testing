e5b393f3721a74f2e7c942a9424b2a447f1f55d6
yemmi408 <ibrahim.malik.ipm@gmail.com>
2025-04-07 23:20:57 +0500

    Revert configuration changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -155,16 +154,7 @@ export default defineConfig({
       },
     },
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: true,
-  },
   server: {
-    port: 3000,
+    allowedHosts: true,
   },
 });
